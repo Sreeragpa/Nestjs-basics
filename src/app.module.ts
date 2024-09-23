2e133130a64bd2928016c01520e0a2522236357c
Sreerag PA <catchsreerag@gmail.com>
2024-09-23 21:46:59 +0530

    Middleware added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggingMiddleware } from './logging.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggingMiddleware)
+      .forRoutes(AppController)
+      // .forRoutes({ path: 'cats', method: RequestMethod.GET }); Another way to apply middleware for specific path and method
+  }
+  
+}
+
+
+// There is no place for middleware in the @Module() decorator. Instead, we set them up using the configure() method of the module class.
+    // Modules that include middleware have to implement the NestModule interface. 
+    // Let's set up the LoggerMiddleware at the AppModule level.
